98cce93ee3d574ff4691ef20c26ad25302ecaa1c
Parinaz <parinaz.kapourchian@gmail.com>
2024-09-30 21:32:16 +0330

    Align Center Title and Items of Contact List

--- contactList/src/components/ContactItem.jsx
+++ contactList/src/components/ContactItem.jsx
@@ -1,13 +1,13 @@
 export const ContactItem = ({ name, phone, email }) => {
   return (
-    <div className="w-2/4 flex justify-around">
-      <h3>
+    <div className="w-full flex justify-around">
+      <h3 className="w-4/12 p-4 text-center">
         {name} 
       </h3>
-      <h3>
+      <h3 className="w-4/12 p-4 text-center">
         {phone} 
       </h3>
-      <h3>
+      <h3 className="w-4/12 p-4 text-center">
         {email}
       </h3>
     </div>
--- contactList/src/components/ContactList.jsx
+++ contactList/src/components/ContactList.jsx
@@ -3,26 +3,28 @@ import { ContactItem } from "./ContactItem";
 export const ContactList = ({ contactItems }) => {
   return (
     <div>
-      <div className="w-2/4 flex justify-around pt-8 pb-4 border-b-2">
-        <div className="flex w-full justify-center">
+      <div className="w-full flex justify-around pt-8 pb-4 border-b-2">
+        <div className="flex w-4/12 justify-center font-bold">
           <span className="material-symbols-outlined">person</span>
           <h3>Name</h3>
         </div>
 
-        <div className="flex w-full justify-center">
+        <div className="flex w-4/12 justify-center font-bold">
           <span className="material-symbols-outlined">call</span>
           <h3>Phone</h3>
         </div>
 
-        <div className="flex w-full justify-center">
+        <div className="flex w-4/12 justify-center font-bold">
           <span className="material-symbols-outlined">mail</span>
           <h3>Email</h3>
         </div>
       </div>
 
+      <div>
       {contactItems.map(({ id, name, phone, email }) => (
         <ContactItem key={id} name={name} phone={phone} email={email} />
       ))}
+      </div>
     </div>
   );
 };
